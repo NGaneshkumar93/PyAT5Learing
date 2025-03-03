3d62a2089d7a17683e08fddfae9fadf1d0a50145
windows <ganeshkumarnarayanan75@gmail.com>
2025-03-03 23:35:22 +0530

    03 Mar

---
+++ N1/O1_Python_Basics/Lab003_Basics1.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("Ganesh kumar")
+print("John", 234, "GK")
+print("John", 234, "GK", "John")
+# Python is sensitive to intentation. In case of error, right click and do reformat.
+print("John", 234, "GK",sep="-")
+print("John", 234, "GK",sep="-",end="**")
+print("John", 234, "GK", "John")
